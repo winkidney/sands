84131cfab9fa614d298f9782df84788cc8102951
winkidney <winkidney@gmail.com>
2015-11-25 09:53:35 -0800

    update package info

--- sand_maker/setup.py
+++ sand_maker/setup.py
@@ -8,7 +8,6 @@ here = os.path.abspath(os.path.dirname(__name__))
 setup(
     name='sand-maker',
     version='0.1',
-    packages=['sand_maker.words', 'sand_maker.words.migrations', 'sand_maker.sand_maker'],
     url='http://github.com/winkidney/sand-maker',
     license='GPL v3',
     packages=find_packages(here),
@@ -17,5 +16,6 @@ setup(
     description='Make sentence from words, enjoy and make new story.',
     requires=[
         "django-registration-defaults",
+        "django-rest-framewrok"
     ]
 )
